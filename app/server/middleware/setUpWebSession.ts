be6a7cd899210e5f1c32046431e267c97ba2b402
Daniel Burnley <daniel.burnley@digital.justice.gov.uk>
2023-06-27 12:54:16 +0100

    Make PoC work localy without external repos (#25)
    * Get PoC running locally with one app
    
    * Move app to subdirectory
    
    * Add components app
    
    This is a clone of the hmpps typescript template on the hmpps session
    branch at time of writing
    
    * Make docker-compose use new app
    
    * Move husky to top level directory
    
    * Header component with parametised service name
    
    * Use application name variable for header
    
    * Set config variables
    
    * Change version of library depended on
    
    * Commit in case of crash
    
    * Update to latest version of session store on main
    
    * Update services to use new central session library on NPM
    
    * Update build pipeline
    
    * Update CI config
    
    * Update CI config
    
    * Update CI config
    
    * Update CI config
    
    * Update CI config
    
    * Update CI config
    
    * Remove console logs

---
+++ app/server/middleware/setUpWebSession.ts
@@ -0,0 +1,46 @@
+import { v4 as uuidv4 } from 'uuid'
+import { hmppsSession } from '@ministryofjustice/hmpps-central-session'
+import express, { Router } from 'express'
+import { createRedisClient } from '../data/redisClient'
+import config from '../config'
+import logger from '../../logger'
+
+export default function setUpWebSession(): Router {
+  const client = createRedisClient()
+  client.connect().catch((err: Error) => logger.error(`Error connecting to Redis`, err))
+
+  const router = express.Router()
+  router.use(
+    hmppsSession(client, {
+      serviceName: 'poc-app',
+      https: config.https,
+      session: { secret: config.session.secret },
+      sharedSession: {
+        host: config.sharedRedis.host,
+        password: config.sharedRedis.password,
+        port: config.sharedRedis.port,
+        tls_enabled: config.sharedRedis.tls_enabled,
+      },
+    }),
+  )
+
+  // Update a value in the cookie so that the set-cookie will be sent.
+  // Only changes every minute so that it's not sent with every request.
+  router.use((req, res, next) => {
+    req.session.nowInMinutes = Math.floor(Date.now() / 60e3)
+    next()
+  })
+
+  router.use((req, res, next) => {
+    const headerName = 'X-Request-Id'
+    const oldValue = req.get(headerName)
+    const id = oldValue === undefined ? uuidv4() : oldValue
+
+    res.set(headerName, id)
+    req.id = id
+
+    next()
+  })
+
+  return router
+}
