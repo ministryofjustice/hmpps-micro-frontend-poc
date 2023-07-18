3bd109c1c434cf3eb77d981bba28a478aadb2dc3
Matthew Whitfield <matthew.whitfield@digital.justice.gov.uk>
2023-07-18 09:15:55 +0100

    import ps

--- server/app.ts
+++ server/app.ts
@@ -22,7 +22,10 @@ import type { Services } from './services'
 
 export default function createApp(services: Services): express.Application {
   const app = express()
-
+  app.use((req, res, next) => {
+    res.locals.basedir = __dirname
+    next()
+  })
   app.set('json spaces', 2)
   app.set('trust proxy', true)
   app.set('port', process.env.PORT || 3000)
