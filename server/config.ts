b8542f0676d6d0e2d7ea996794c97526934f758e
Daniel Burnley <daniel.burnley@digital.justice.gov.uk>
2023-07-27 14:20:09 +0100

    Add library (#31)
    * Update dependencies
    
    * Add library
    
    * Fix e2e tests
    
    * Update library
    
    * linting
    
    * Add session URL

--- server/config.ts
+++ server/config.ts
@@ -66,6 +66,9 @@ export default {
       agent: new AgentConfig(Number(get('TOKEN_VERIFICATION_API_TIMEOUT_RESPONSE', 5000))),
       enabled: get('TOKEN_VERIFICATION_ENABLED', 'false') === 'true',
     },
+    session: {
+      url: get('SESSION_API_URL', 'http://localhost:8100', requiredInProduction),
+    },
   },
   domain: get('INGRESS_URL', 'http://localhost:3000', requiredInProduction),
 }
--- server/middleware/setUpWebSession.ts
+++ server/middleware/setUpWebSession.ts
@@ -1,28 +1,26 @@
+import { hmppsSessionBuilder } from '@ministryofjustice/hmpps-central-session'
 import { v4 as uuidv4 } from 'uuid'
-import session from 'express-session'
-import connectRedis, { Client } from 'connect-redis'
 import express, { Router } from 'express'
 import { createRedisClient } from '../data/redisClient'
 import config from '../config'
 import logger from '../../logger'
 
-const RedisStore = connectRedis(session)
-
 export default function setUpWebSession(): Router {
-  const client = createRedisClient({ legacyMode: true })
+  const client = createRedisClient()
   client.connect().catch((err: Error) => logger.error(`Error connecting to Redis`, err))
+  const options = {
+    cookie: { secure: config.https, maxAge: 120 * 60 * 60 },
+    sessionSecret: config.session.secret,
+    sharedSessionApi: {
+      baseUrl: config.apis.session.url,
+      token: 'SOME_TOKEN',
+    },
+  }
+
+  const sessionBuilder = hmppsSessionBuilder(client, options)
 
   const router = express.Router()
-  router.use(
-    session({
-      store: new RedisStore({ client: client as unknown as Client }),
-      cookie: { secure: config.https, sameSite: 'lax', maxAge: config.session.expiryMinutes * 60 * 1000 },
-      secret: config.session.secret,
-      resave: false, // redis implements touch so shouldn't need this
-      saveUninitialized: false,
-      rolling: true,
-    }),
-  )
+  router.use(sessionBuilder('hmpps-micro-frontend-poc'))
 
   // Update a value in the cookie so that the set-cookie will be sent.
   // Only changes every minute so that it's not sent with every request.
