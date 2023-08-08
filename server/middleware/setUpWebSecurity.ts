b03cbb86ae7381c330ea91b2f2ee30a2725192b7
Daniel Burnley <daniel.burnley@digital.justice.gov.uk>
2023-08-08 16:56:31 +0100

    Add csp url (#42)

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -1,6 +1,7 @@
 import express, { Router, Request, Response, NextFunction } from 'express'
 import helmet from 'helmet'
 import crypto from 'crypto'
+import config from '../config'
 
 export default function setUpWebSecurity(): Router {
   const router = express.Router()
@@ -12,19 +13,28 @@ export default function setUpWebSecurity(): Router {
     res.locals.cspNonce = crypto.randomBytes(16).toString('hex')
     next()
   })
+
+  // This nonce allows us to use scripts with the use of the `cspNonce` local, e.g (in a Nunjucks template):
+  // <script nonce="{{ cspNonce }}">
+  // or
+  // <link href="http://example.com/" rel="stylesheet" nonce="{{ cspNonce }}">
+  // This ensures only scripts we trust are loaded, and not anything injected into the
+  // page by an attacker.
+  const scriptSrc = ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`]
+  const styleSrc = ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`]
+
+  if (config.apis.component.url) {
+    scriptSrc.push(config.apis.component.url)
+    styleSrc.push(config.apis.component.url)
+  }
+
   router.use(
     helmet({
       contentSecurityPolicy: {
         directives: {
           defaultSrc: ["'self'"],
-          // This nonce allows us to use scripts with the use of the `cspNonce` local, e.g (in a Nunjucks template):
-          // <script nonce="{{ cspNonce }}">
-          // or
-          // <link href="http://example.com/" rel="stylesheet" nonce="{{ cspNonce }}">
-          // This ensures only scripts we trust are loaded, and not anything injected into the
-          // page by an attacker.
-          scriptSrc: ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`],
-          styleSrc: ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`],
+          scriptSrc,
+          styleSrc,
           fontSrc: ["'self'"],
         },
       },
